e3dbd2f0a4378382ddfce2317da2efa34884d4fc
laurahw <lhanna-white@andigital.com>
2018-07-16 16:29:40 +0100

    :construction: Adding text descriptions for different moods

--- src/Smilies.js
+++ src/Smilies.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import descriptions from './data/desc.json'
 
 
 class Smilies extends Component {
+
+  constructor(props){
+    super()
+
+    this.state = {
+      descriptionText: "What kind of mood are you in?"
+    }
+  }
+
+
+
+  clickHandler(mood){
+      this.setState({descriptionText:  descriptions[mood].description})
+  }
+
   render() {
     return (
       <div className='smilies'>
         <div className='text'>How are you feeling today?</div>
         <div className='row'>
-          <FontAwesomeIcon icon="smile" className='icon happy'/>
-        <FontAwesomeIcon icon="meh" className='icon okay'/>
-      <FontAwesomeIcon icon="frown" className='icon sad'/>
-
+          <FontAwesomeIcon icon="smile" className='icon happy' onClick={() => this.clickHandler(0)}/>
+        <FontAwesomeIcon icon="meh" className='icon okay' onClick={() => this.clickHandler(1)}/>
+      <FontAwesomeIcon icon="frown" className='icon sad' onClick={() => this.clickHandler(2)}/>
+        </div>
+        <div className="description">
+          {this.state.descriptionText}
         </div>
       </div>
-
     );
   }
 }
